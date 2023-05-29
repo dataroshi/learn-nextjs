900a62aa9cb159fc7340bcf1601c3c2087145235
dataroshi <dataroshi@gmail.com>
2023-05-29 14:01:31 +0200

    ISR add the service get level

--- part4/lib/getUserPosts.ts
+++ part4/lib/getUserPosts.ts
@@ -1,5 +1,6 @@
 export default async function getUserPosts(userId: String) {
-    const res = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`);
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`,
+        {next: {revalidate: 60 }});
 
     if (!res.ok) {
         throw new Error('Failed fetch user posts');
